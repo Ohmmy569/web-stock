3e9a476067c332e6adfd42d7c09034a3f58f1f0d
Worawiboon mungmee <Ohmmy569@gmail.com>
2024-04-09 04:17:50 +0700

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AP-Stock",
+  title: "AP-Sssstock",
   description: "Web app for managing stock",
 };
 
