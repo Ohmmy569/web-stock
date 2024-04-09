4819a62e77965ac1078c0001ce0462312550ac02
Worawiboon mungmee <Ohmmy569@gmail.com>
2024-04-09 11:09:40 +0700

    mantine provide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+//layout.tsx
+import '@mantine/core/styles.css';
+import React from 'react';
+import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "AP-Sssstock",
-  description: "Web app for managing stock",
+export const metadata = {
+  title: 'AP-Stock',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <ColorSchemeScript />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
