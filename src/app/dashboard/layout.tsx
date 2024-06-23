87fd08a1e7e314339c610384a953eb32e29252e1
Worawiboon mungmee <Ohmmy569@gmail.com>
2024-06-23 18:04:51 +0700

    pic and more

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   AppShell,
   Burger,
@@ -12,6 +12,8 @@ import {
   NavLink,
   Box,
   Center,
+  Image,
+  Text,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import {
@@ -19,82 +21,146 @@ import {
   IconCar,
   IconEngine,
   IconTablePlus,
+  IconLogout,
+  IconUser,
 } from "@tabler/icons-react";
 import classes from "@css/nav.module.css";
 
+import { usePathname } from "next/navigation";
+
 export default function DashboardLayout({
-  children, // will be a page or nested layout
+  children,
 }: {
   children: React.ReactNode;
 }) {
   const [opened, { toggle }] = useDisclosure();
+
   const data = [
-    { link: "/dashboard/parts", label: "รายการอะไหล่", icon: IconEngine },
     {
-      link: "/dashboard/parts/page.tsx",
-      label: "รายการเบิกอะไหล่",
+      link: "/dashboard/parts",
+      label: 
+          "รายการอะไหล่รถยนต์"
+        
+      ,
+      icon: IconEngine,
+    },
+    {
+      link: "/dashboard/outparts",
+      label: 
+          "รายการอะไหล่ที่เบิก"
+      ,
       icon: IconShoppingCartCog,
     },
-    { link: "/dashboard/car", label: "รายการรถยนต์", icon: IconCar },
     {
-      link: "/dashboard/parts/page.tsx",
-      label: "รายการเพิ่มอะไหล่",
+      link: "/dashboard/car",
+      label: 
+          "รายการรถยนต์"
+      ,
+      icon: IconCar,
+    },
+    {
+      link: "/dashboard/addparts",
+      label: 
+          "เพิ่มอะไหล่รถยนต์"
+      ,
       icon: IconTablePlus,
     },
   ];
-  const [active, setActive] = useState(0);
 
-    const handleClick = (event : any, index : any)  => {
-    event.preventDefault();
-    setActive(index);
-
+  const handleClick = (event: any, index: any) => {
     window.location.href = data[index].link;
-    
+    event.preventDefault();
+  };
 
-  }
-
-  const items = data.map((item, index) => (
-    <NavLink
-      href={item.link}
-      key={item.label}
-      active={index === active}
-      label={item.label}
-      leftSection={<item.icon size="2rem" stroke={2} />}
-      onClick={(event) => handleClick(event, index)}
-      color={"#4746ED"}
-      fw={900}
-       // ตั้งค่าขนาดตัวอักษรที่ใหญ่ขึ้น
-    />
-  ));
-  
+  const pathname = usePathname();
+  const username = "Apcar";
 
   return (
     <section>
       <AppShell
-        header={{ height: 60 }}
+        header={{ height: 55 }}
         navbar={{
-          width: 250,
+          width: 240,
           breakpoint: "sm",
           collapsed: { mobile: !opened },
         }}
         padding="sm"
       >
         <AppShell.Header bg={"myblue"}>
-          <Group h="100%" px="md">
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              hiddenFrom="sm"
-              size="md"
-              c={"white"}
-              color="white"
+          <Group h="100%" px="xs" justify="space-between" gap="xl">
+            <Group>
+              <Burger
+                opened={opened}
+                onClick={toggle}
+                hiddenFrom="sm"
+                size="md"
+                c={"white"}
+                color="white"
+              />{" "}
+            </Group>
+
+            <Image
+              alt="Banner"
+              w="100%"
+              h={54}
+              radius="md"
+              src="/navicon.png"
+              m={0}
             />
+            <Text> </Text>
           </Group>
         </AppShell.Header>
-        <AppShell.Navbar >
-          <Box >{items}</Box>
-        
-          
+
+        <AppShell.Navbar>
+          <AppShell.Section grow>
+            <Box>
+              {data.map((item, index) => {
+                const isActive = pathname.startsWith(item.link);
+
+                return (
+                  <NavLink
+                    href={item.link}
+                    key={index}
+                    active={isActive}
+                    label={item.label}
+                    leftSection={<item.icon size="2rem" stroke={2} />}
+                    onClick={(event) => handleClick(event, index)}
+                    color={"#4746ED"}
+                    fw={900}
+                    p={10}
+            
+                  />
+                );
+              })}
+            </Box>
+          </AppShell.Section>
+          <AppShell.Section pb={5}>
+            <Divider p={5} size={2} />
+            <Box>
+            <NavLink
+                label={username}
+                leftSection={<IconUser size="2rem" stroke={2} />}
+                // onClick={}
+                color={"myblue"}
+                c={"myblue"}
+                fw={900}
+                p={10}
+                className={classes.navlink}
+         
+              />
+              <NavLink
+                label={"ออกจากระบบ"}
+                leftSection={<IconLogout size="2rem" stroke={2} />}
+                // onClick={}
+                color={"red"}
+                c={"red"}
+                
+                fw={900}
+                p={10}
+           
+              />
+            </Box>
+          </AppShell.Section>
         </AppShell.Navbar>
         <AppShell.Main> {children}</AppShell.Main>
       </AppShell>
