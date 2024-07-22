3f187db5acf7d58ab92f13fe53b27f10d7cd5ee6
Worawiboon mungmee <Ohmmy569@gmail.com>
2024-07-23 03:37:50 +0700

    refactor: Update fetch URL in PartTypeTable

--- src/app/components/PartTypeTable.tsx
+++ src/app/components/PartTypeTable.tsx
@@ -39,7 +39,7 @@ const PartTypeTable = () => {
     useDisclosure(false);
 
   const fetchPartType = async () => {
-    const res = await fetch("http://localhost:3000/api/typeofparts", {
+    const res = await fetch("/api/typeofparts", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -68,7 +68,7 @@ const PartTypeTable = () => {
 
   const filteredParts = PartType?.filter((PartType: PartType) => {
     const searchFields = Object.values(PartType).join("").toLowerCase();
-    
+
 
     return searchFields.includes(search.toLowerCase());
   });
